dcbe940aafe3b74295292c9e3cd467860cc4c942
Curtis Laurence Chadwell <43461325+curchadw@users.noreply.github.com>
2021-03-28 04:57:48 +0000

    I explained my code

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-//
+//I start out with an empty array
 const myStudies = []
 
+//This is how I am add subjects dynamically instead of hardcoding a bunck of variables
 const addStudies = subject =>{
   myStudies.includes(subject) === false ? myStudies.push(subject) : console.log('Already exist in collection')
   console.log(myStudies)
   return myStudies
 }
 
+//This fuction will log each subject individually
 const listSubjects = () =>{
   for(let i = 0; i < myStudies.length; i++){
     console.log(myStudies[i])
-    return myStudies[i] || 'Empty'
+    // return myStudies[i] || 'Empty'
   }
   
 }
 
 
-
+//This function will tell you how I feel about other subject and how I feel about my favorite subject
 const favoriteStudy = (subject) =>{
    if(subject === myStudies[0]){
     console.log('One of my favorite subjects')
